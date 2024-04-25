282f5bf08de6d714fd6b10e2649a6ed51f67ab9d
AchiGel <ar.gelashvili@gmail.com>
2024-04-25 13:59:22 +0400

    add initial compinents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import styled from "styled-components";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Main from "./components/Main";
+
+const Wrapper = styled.div`
+  width: 80%;
+  max-width: 1280px;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Form />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
