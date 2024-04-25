89e4d808a3f54d05f6d4fb161389bf7031bcab16
AchiGel <ar.gelashvili@gmail.com>
2024-04-25 14:11:53 +0400

    add react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
+import { IoMoonSharp, IoMoonOutline } from "react-icons/io5";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 3em;
+  border-bottom: 1px solid var(--dark-blue);
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderContainer>
       Where In The World?
-      <div>Dark Mode</div>
-    </header>
+      <div>
+        <IoMoonOutline /> Dark Mode
+      </div>
+    </HeaderContainer>
   );
 }
