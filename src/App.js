e0eaea069affbb5004df1293155d4c94fabb2822
AchiGel <ar.gelashvili@gmail.com>
2024-04-25 16:37:38 +0400

    add dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import styled from "styled-components";
 import Header from "./components/Header";
 import Form from "./components/Form";
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,36 @@
+import { useEffect, useState } from "react";
 import { IoMoonSharp, IoMoonOutline } from "react-icons/io5";
 import styled from "styled-components";
+import "../index.css";
 
 const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
   height: 3em;
-  border-bottom: 1px solid var(--dark-blue);
+  box-shadow: var(--shadow);
 `;
 
+const Title = styled.a``;
+
+const Switcher = styled.div``;
+
 export default function Header() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-body");
+  }, [theme]);
   return (
     <HeaderContainer>
-      Where In The World?
-      <div>
+      <Title>Where In The World?</Title>
+      <Switcher onClick={toggleTheme}>
         <IoMoonOutline /> Dark Mode
-      </div>
+      </Switcher>
     </HeaderContainer>
   );
 }
